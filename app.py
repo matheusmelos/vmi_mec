49010f4aae487499e69bfdd2eae5e8e3817648a9
MatheusMelo <mtheusmelo.s@gmail.com>
2024-12-07 21:15:41 -0300

    update: change functions for new class

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from models.zip_manager import ZipFolderManager
+from models.pdf_impress import PDF_Printer
 import os
 from flask import Flask, render_template, request, send_file, jsonify
+
 
 app = Flask(__name__)
 
@@ -16,6 +18,16 @@ def process_zip(file_path):
     # Return the path of the processed zip file
     return processor.processed_zip
 
+
+# Function that processes the zip file using FolderProcessor
+def process(file_path):
+    # Create FolderProcessor object to process the file
+    processor = PDF_Printer(file_path)
+    processor.clean_all()
+    # Return the path of the processed zip file
+    return processor.processed_zip
+
+
 # Home route with upload form
 @app.route('/')
 def index():
@@ -25,6 +37,27 @@ def index():
 @app.route('/admin')
 def admin_page():
     return render_template('adminPage.html')
+
+
+
+# Route to handle the file upload and processing
+@app.route('/upload_file_processed', methods=['POST'])
+def upload_file_processed():
+    if 'file' not in request.files:
+        return jsonify({'success': False, 'message': 'No file part'})
+    
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'success': False, 'message': 'No selected file'})
+    
+    if file:
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(file_path)
+        
+        # Process the uploaded zip file using FolderProcessor
+        processed_zip_path = process(file_path)
+        
+        return jsonify({'success': True, 'filename': os.path.basename(processed_zip_path)})
 
 # Route to handle the file upload and processing
 @app.route('/upload', methods=['POST'])
--- static/js/script.js
+++ static/js/script.js
@@ -25,7 +25,41 @@ function uploadFile() {
         .catch(error => {
             console.error('Erro:', error);
         })
-        .finally(() => {
+        .finally(() => { 
+            // Ocultar tela de carregamento
+            document.getElementById('loading').style.display = 'none';
+        });
+    };
+}
+
+function uploadFileProcessed() {
+    const fileInput = document.getElementById('fileInput');
+    fileInput.click();
+
+    fileInput.onchange = function() {
+        const formData = new FormData();
+        formData.append('file', fileInput.files[0]);
+    
+        // Mostrar tela de carregamento
+        document.getElementById('loading').style.display = 'block';
+    
+        fetch('/upload_file_processed', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                console.log("opa")
+                window.location.href = `download_file/${data.filename}`;
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        })
+        .finally(() => { 
             // Ocultar tela de carregamento
             document.getElementById('loading').style.display = 'none';
         });
